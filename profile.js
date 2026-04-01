619dfe0fb4939f9d20f995fbbed916182f5d946e
xhkdm1 <your-xhkdm1116@gmail.com>
2026-04-01 11:31:10 +0900

    이메일 정보 전송

---
+++ profile.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('form');
+const submitButton = document.querySelector('#sendEmail');
+const dimOverlay = document.querySelector('#dimOverlay');
+
+const sendEmail = () => {
+  // 스피너와 dim 화면 표시
+  dimOverlay.classList.add('on');
+
+  emailjs
+    .sendForm('service_j44ao5m', 'template_v1zd1s2', form, {
+      // publicKey: 'emailjs에서 발급받은 사용자 키를 입력하세요',
+    })
+    .then(
+      () => {
+        // 스피너와 dim 화면 숨기기
+        dimOverlay.classList.remove('on');
+        alert(`${form.from_name.value}님, 메일 전송에 성공했습니다 :)`);
+        // 폼 리셋
+        form.reset();
+      },
+      (error) => {
+        // 스피너와 dim 화면 숨기기
+        dimOverlay.classList.remove('on');
+        alert(`${form.from_name.value}님, 메일 전송에 실패했습니다 :(`);
+        console.error('메일 전송 실패:', error);
+      },
+    );
+};
+
+// 버튼 클릭 시 sendEmail 함수 호출
+submitButton.addEventListener('click', sendEmail);
